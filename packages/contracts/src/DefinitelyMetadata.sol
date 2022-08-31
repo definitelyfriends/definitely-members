5bb39f3c69d5df4b3dfbfaa3c94e9b118024623c
Sam King <mail@samking.co>
2022-08-31 12:15:01 +0100

    Update basic metadata contract

--- packages/contracts/src/DefinitelyMetadata.sol
+++ packages/contracts/src/DefinitelyMetadata.sol
@@ -23,26 +23,47 @@ pragma solidity ^0.8.15;
 
 import "@solmate/auth/Owned.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
-import {IDefinitelyMetadata} from "./interfaces/IDefinitelyMetadata.sol";
+import "@def/Base64.sol";
+import "./interfaces/IDefinitelyMetadata.sol";
 
 /// @title Definitely Metadata
 /// @author DEF DAO
-/// @notice The default metadata contract for DEF DAO membership tokens, a simple baseURI style
-///         implementation that allows the owner to update the baseURI.
+/// @notice The default metadata contract for DEF DAO membership tokens. On-chain metadata
+///         with off-chain image URI that's updateable by the contract owner.
 contract DefinitelyMetadata is IDefinitelyMetadata, Owned {
     using Strings for uint256;
 
-    string public baseURI;
+    string public imageURI;
+    string public description;
 
-    constructor(address owner, string memory baseURI_) Owned(owner) {
-        baseURI = baseURI_;
+    constructor(address owner, string memory imageURI_) Owned(owner) {
+        imageURI = imageURI_;
+        description = "[DEF DAO](https://www.defdao.xyz/) membership token for people tinkering on the edge of the internet.";
     }
 
-    function setBaseURI(string memory baseURI_) external onlyOwner {
-        baseURI = baseURI_;
+    function setImageURI(string memory imageURI_) external onlyOwner {
+        imageURI = imageURI_;
+    }
+
+    function setDescription(string memory description_) external onlyOwner {
+        description = description_;
     }
 
     function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
-        return bytes(baseURI).length > 0 ? string.concat(baseURI, tokenId.toString()) : "";
+        string memory json = Base64.encode(
+            bytes(
+                string.concat(
+                    '{"name":"DEF Membership #',
+                    tokenId.toString(),
+                    '","description":"',
+                    description,
+                    '","image":"',
+                    imageURI,
+                    '","attributes":[]}'
+                )
+            )
+        );
+
+        return string.concat("data:application/json;base64,", json);
     }
 }
